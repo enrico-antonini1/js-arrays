cd3e3ccef516bf9d3e9edcced29bfb9cb66f3ddf
enrico-antonini1 <enry1997@me.com>
2025-09-20 14:52:12 +0200

    feat: task 6

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -29,10 +29,12 @@ const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; //
 
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-teachers.push("Vanessa");
-console.log(teachers);
+// teachers.push("Vanessa");
+// console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah");
+console.log(teachers);
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
